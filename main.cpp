d0e1cdfdfb5d063a395a615edc2970865ea9afb8
drainpixie <121581793+drainpixie@users.noreply.github.com>
2023-10-21 13:32:45 +0200

    feat: tidy code

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  auto image = cv::imread(argv[1]);
-  auto ext = std::string(argv[1]).substr(std::string(argv[1]).find_last_of(".") + 1);
+  auto path = std::string(argv[1]);
+  auto image = cv::imread(path);
+  // auto ext = std::string(argv[1]).substr(std::string(argv[1]).find_last_of(".") + 1);
+  auto ext = path.substr(path.find_last_of(".") + 1);
 
   if (image.empty()) {
     std::cerr << "error: could not open image file" << std::endl;
@@ -17,7 +19,6 @@ int main(int argc, char *argv[]) {
 
   // Bayer matrix, https://en.wikipedia.org/wiki/Ordered_dithering
   cv::Mat matrix = (cv::Mat_<float>(4, 4) << 0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7, 13, 5);
-
   matrix /= 16;
 
   for (int y = 0; y < image.rows; y++) {
@@ -28,8 +29,8 @@ int main(int argc, char *argv[]) {
       auto grayscale =
           0.2126 * pixel[2] + 0.7152 * pixel[1] + 0.0722 * pixel[0];
 
-      int threshold = matrix.at<float>(y % 4, x % 4) * 255;
-      int dithered = (grayscale > threshold) ? 255 : 0;
+      auto threshold = matrix.at<float>(y % 4, x % 4) * 255;
+      auto dithered = (grayscale > threshold) ? 255 : 0;
 
       image.at<cv::Vec3b>(y, x) = cv::Vec3b(dithered, dithered, dithered);
     }
