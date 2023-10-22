5e4efd59fc8713c7d5257d5cfc1c52fe06051e0a
drainpixie <121581793+drainpixie@users.noreply.github.com>
2023-10-23 00:20:23 +0200

    feat(cli): add treshold factor

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <unistd.h>
+
+// TODO: cleanup
+
+auto usage(char *argv[]) {
+  std::cerr << "usage: " << argv[0]
+            << " [-t threshold_factor] <input> [output=dithered.ext]"
+            << std::endl;
+  return 1;
+}
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cerr << "usage: " << argv[0] << " <input> [output=dithered.ext]" << std::endl;
+  int opt;
+
+  float threshold_factor = 1.0;
+
+  while ((opt = getopt(argc, argv, "t:")) != -1) {
+    switch (opt) {
+    case 't':
+      threshold_factor = std::atof(optarg);
+      break;
+    default:
+      usage(argv);
+      return 1;
+    }
+  }
+
+  if (optind >= argc) {
+    usage(argv);
     return 1;
   }
 
-  auto path = std::string(argv[1]);
-  auto image = cv::imread(path);
-  // auto ext = std::string(argv[1]).substr(std::string(argv[1]).find_last_of(".") + 1);
+  auto path = std::string(argv[optind]);
   auto ext = path.substr(path.find_last_of(".") + 1);
 
+  auto image = cv::imread(path);
   if (image.empty()) {
     std::cerr << "error: could not open image file" << std::endl;
     return 1;
   }
 
   // Bayer matrix, https://en.wikipedia.org/wiki/Ordered_dithering
-  cv::Mat matrix = (cv::Mat_<float>(4, 4) << 0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7, 13, 5);
+  cv::Mat matrix = (cv::Mat_<float>(4, 4) << 0, 8, 2, 10, 12, 4, 14, 6, 3, 11,
+                    1, 9, 15, 7, 13, 5);
   matrix /= 16;
 
   for (int y = 0; y < image.rows; y++) {
@@ -29,15 +54,16 @@ int main(int argc, char *argv[]) {
       auto grayscale =
           0.2126 * pixel[2] + 0.7152 * pixel[1] + 0.0722 * pixel[0];
 
-      auto threshold = matrix.at<float>(y % 4, x % 4) * 255;
+      auto threshold = matrix.at<float>(y % 4, x % 4) * 255 * threshold_factor;
       auto dithered = (grayscale > threshold) ? 255 : 0;
 
       image.at<cv::Vec3b>(y, x) = cv::Vec3b(dithered, dithered, dithered);
     }
   }
 
-  cv::imwrite(argv[2] ? argv[2] : std::string("dithered.") + ext, image);
+  cv::imwrite((optind + 1 < argc) ? argv[optind + 1] : "dithered." + ext,
+              image);
   image.release();
-  
+
   return 0;
 }
